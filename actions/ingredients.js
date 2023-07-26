81291e2f459e5bef565f1e391efa183702594b01
nmayhew <66178080+nmayhew@users.noreply.github.com>
2023-07-26 09:41:13 +0100

    consts for action types

--- actions/ingredients.js
+++ actions/ingredients.js
@@ -1,7 +1,8 @@
+import { ADD_INGREDIENT } from "../constants/actionTypes";
 // Use Action Creators to simplify structure from user 
 // Can do default values, perform cleanup, name checks etc...
 export const addIngredient = (recipe, name, quantity) => ({
-    type: 'ADD_INGREDIENT',
+    type: ADD_INGREDIENT,
     recipe,
     name,
     quantity
--- actions/recipe.js
+++ actions/recipe.js
@@ -1,6 +1,7 @@
+import { ADD_RECIPE } from "../constants/actionTypes";
 // Use Action Creators to simplify structure from user 
 // Can do default values, perform cleanup, name checks etc...
 export const addRecipe = (name) => ({
-    type: 'ADD_RECIPE',
+    type: ADD_RECIPE,
     name
 });
